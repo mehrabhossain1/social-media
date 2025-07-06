55f131927551dc3cca52e26d3cebc528a23a5442
Mehrab Hossain <mehrab.munna00@gmail.com>
2025-07-06 06:01:24 +0600

    feat: clerk user stored in db

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -1,3 +1,4 @@
+import prisma from "@/lib/client";
 import { verifyWebhook } from "@clerk/nextjs/webhooks";
 import { NextRequest } from "next/server";
 
@@ -13,6 +14,48 @@ export async function POST(req: NextRequest) {
             `Received webhook with ID ${id} and event type of ${eventType}`
         );
         console.log("Webhook payload:", evt.data);
+
+        // You can handle different event types here
+        if (eventType === "user.created") {
+            // Handle user created event
+            console.log("User created:", evt.data);
+            try {
+                await prisma.user.create({
+                    data: {
+                        clerkId: evt.data.id,
+                        username: evt.data.username || "",
+                        avatar: evt.data.image_url || "/noAvatar.png",
+                        cover: "/noCover.png",
+                    },
+                });
+                return new Response("User created successfully", {
+                    status: 200,
+                });
+            } catch (err) {
+                console.log(err);
+                return new Response("Failed to create user!", { status: 500 });
+            }
+        }
+        if (eventType === "user.updated") {
+            // Handle user created event
+            console.log("User updated:", evt.data);
+            try {
+                await prisma.user.update({
+                    where: { clerkId: evt.data.id },
+                    // Update only the fields that are present in the event data
+                    data: {
+                        username: evt.data.username || "",
+                        avatar: evt.data.image_url || "/noAvatar.png",
+                    },
+                });
+                return new Response("User updated successfully", {
+                    status: 200,
+                });
+            } catch (err) {
+                console.log(err);
+                return new Response("Failed to update user!", { status: 500 });
+            }
+        }
 
         return new Response("Webhook received", { status: 200 });
     } catch (err) {
